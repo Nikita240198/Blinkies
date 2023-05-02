61c307990bece9a31e017b6e48f6714043d43b85
Nikita <nikitabargalmindruby@gmail.com>
2023-05-02 18:01:59 +0530

    second commit

--- src/test/java/Test1/VerifyTitleAndText.java
+++ src/test/java/Test1/VerifyTitleAndText.java
@@ -1,24 +1,38 @@
 package Test1;
 
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.testng.Assert;
 import org.testng.annotations.Test;
+import org.testng.asserts.SoftAssert;
 
-public class VerifyTestTitle {
+public class VerifyTitleAndText {
 	@Test
 	
 	public void titletest() {
 		
 		String expectedtitle = "Blinkly";
 		
+		String expectedtext= "Access";
+		
 		System.setProperty("webdriver.chrome.driver", "/Users/Mind/Downloads/chromedriver_mac64/chromedriver");
 
 		WebDriver driver = new ChromeDriver();
 		driver.get("https://staging.blinkly.com/");
+		
+		SoftAssert softAssert = new SoftAssert();
+		
 		String actualtitle = driver.getTitle();
 		
-		Assert.assertEquals(actualtitle, expectedtitle);
+		String actualtext = driver.findElement (By.cssSelector("[id='accesscontainer']")).getAttribute("class");
+		System.out.println("check access"+ actualtext);
+		
+		
+		softAssert.assertEquals(actualtitle, expectedtitle);
+		softAssert.assertEquals(actualtext,expectedtext);
+		
+		softAssert.assertAll();
 		
 		driver.close();
 	}
